cc4452e06809283b7cdbe458fec127526f3ca078
Dragiln <wilmakomu@gmail.com>
2023-06-22 10:44:26 +0300

    feat: videoplayer

--- src/navigation/HomeNavigation.jsx
+++ src/navigation/HomeNavigation.jsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 
 import HomeScreen from '../screens/HomeScreen';
 import PomodoroScreen from '../screens/PomodoroScreen';
+import VideoScreen from '../screens/Videoscreen';
 
 const Drawer = createDrawerNavigator();
 
@@ -18,6 +19,10 @@ export default function HomeNavigation() {
 				<Drawer.Screen
 					name="Pomodoro"
 					component={PomodoroScreen}
+				/>
+				<Drawer.Screen
+					name="Videos"
+					component={VideoScreen}
 				/>
 			</Drawer.Navigator>
 		</NavigationContainer>
